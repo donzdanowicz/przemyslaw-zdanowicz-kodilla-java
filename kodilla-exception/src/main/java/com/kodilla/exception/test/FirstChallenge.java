73f0d7d8212d2e4e249ce212ae4fca418ef2edbb
donzdanowicz <madpz89@gmail.com>
2021-09-24 18:28:23 +0200

    Task 8.2 corrected

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,19 +2,27 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
     public double divide(double a, double b) throws ArithmeticException {
-
-        try {
-            if (b == 0) {
+        if (b == 0) {
             throw new ArithmeticException();
         }
-            return a/b;
-        } catch (ArithmeticException e) {
-            System.out.println("Something went wrong. Error: " + e);
-        } finally {
-            System.out.println("Never divide by zero!");
-        }
-        return 0;
+        return a / b;
     }
-}
+
+        public static void main(String[] args) {
+            FirstChallenge firstChallenge = new FirstChallenge();
+
+            try {
+                double result = firstChallenge.divide(3, 0);
+                System.out.println(result);
+            } catch (ArithmeticException e) {
+                System.out.println("Something went wrong. Error: " + e);
+            } finally {
+                System.out.println("Remember! Never divide by zero!");
+            }
+        }
+
+    }
+
+
 
 
