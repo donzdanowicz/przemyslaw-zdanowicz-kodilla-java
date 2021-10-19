e74b2cdae6335b7eb958c5ef27f76ed695932452
donzdanowicz <madpz89@gmail.com>
2021-10-19 19:10:20 +0200

    Task 13.5 ver.2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flights/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flights/Application.java
@@ -1,37 +1,27 @@
 package com.kodilla.good.patterns.flights;
 
-import java.util.Scanner;
+import java.util.List;
 import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
         FlightDatabaseRetriever databaseRetriever = new FlightDatabaseRetriever();
         FlightFinder flightFinder = databaseRetriever.retrieve();
-        Airport GDA = new Airport(1, "Gdańsk");
-        Airport WAR = new Airport(2, "Warszawa");
-        Airport WRO = new Airport(3, "Wrocław");
-        Airport POZ = new Airport(4, "Poznań");
 
         System.out.println("\nDirect Flights: \n");
-        System.out.println("to POZ");
-        Set<Flight> foundFlightsTo = flightFinder.findDirectFlightsTo(POZ);
+
+        System.out.println("Flights to POZ");
+        Set<Flight> foundFlightsTo = flightFinder.findDirectFlightsTo("POZ");
         System.out.println(foundFlightsTo);
-        System.out.println("from WRO");
-        Set<Flight> foundFlightsFrom = flightFinder.findDirectFlightsFrom(WRO);
+        System.out.println("Flights from WRO");
+        Set<Flight> foundFlightsFrom = flightFinder.findDirectFlightsFrom("WRO");
         System.out.println(foundFlightsFrom);
-        System.out.println("from WRO to POZ");
-        Set<Flight> foundFlightsFromTo = flightFinder.findDirectFlightsFromTo(GDA, POZ);
+        System.out.println("Flights from GDA to POZ");
+        Set<Flight> foundFlightsFromTo = flightFinder.findDirectFlightsFromTo("GDA", "POZ");
         System.out.println(foundFlightsFromTo);
 
-        System.out.println("\nTransfer Flights: \n");
-        System.out.println("to WRO");
-        Set<Flight> foundTransferFlightsTo = flightFinder.findTransferFlightsTo(WRO);
-        System.out.println(foundTransferFlightsTo);
-        System.out.println("from GDA");
-        Set<Flight> foundTransferFlightsFrom = flightFinder.findTransferFlightsFrom(GDA);
+        System.out.println("\nTransfer flights from GDA to WRO");
+        Set<List<Flight>> foundTransferFlightsFrom = flightFinder.findTransfer("GDA", "WRO");
         System.out.println(foundTransferFlightsFrom);
-        System.out.println("From GDA to WRO");
-        Set<Flight> foundTransferFlightsFromTo = flightFinder.findTransferFlightsFromTo(GDA, WRO);
-        System.out.println(foundTransferFlightsFromTo);
     }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flights/Flight.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flights/Flight.java
@@ -1,48 +1,27 @@
 package com.kodilla.good.patterns.flights;
 
-import java.time.LocalDateTime;
 import java.util.*;
 
 public class Flight {
-    private int number;
-    private Airport departureAirport;
-    private Airport arrivalAirport;
-    private LocalDateTime departureDate;
-    private LocalDateTime arrivalDate;
-    private Set<Flight> flights = new HashSet<>();
+    private final String departureAirport;
+    private final String arrivalAirport;
 
-    public Flight(int number, Airport departureAirport, Airport arrivalAirport, LocalDateTime departureDate, LocalDateTime arrivalDate) {
-        this.number = number;
+    public Flight(String departureAirport, String arrivalAirport) {
         this.departureAirport = departureAirport;
         this.arrivalAirport = arrivalAirport;
-        this.departureDate = departureDate;
-        this.arrivalDate = arrivalDate;
     }
 
-    public int getNumber() {
-        return number;
-    }
-
-    public Airport getDepartureAirport() {
+    public String getDepartureAirport() {
         return departureAirport;
     }
 
-    public Airport getArrivalAirport() {
+    public String getArrivalAirport() {
         return arrivalAirport;
-    }
-
-    public LocalDateTime getDepartureDate() {
-        return departureDate;
-    }
-
-    public LocalDateTime getArrivalDate() {
-        return arrivalDate;
     }
 
     @Override
     public String toString() {
-        return "\nFlight #" + number + " from " + departureAirport.getLocation() + " to " + arrivalAirport.getLocation() + ":\n"
-                + "Departure date: " + departureDate + ". Arrival date: " + arrivalDate + ".";
+        return "Flight from " + departureAirport + " to " + arrivalAirport;
     }
 
     @Override
@@ -50,14 +29,12 @@ public class Flight {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Flight flight = (Flight) o;
-        return number == flight.number && departureAirport.equals(flight.departureAirport)
-                && arrivalAirport.equals(flight.arrivalAirport) && departureDate.equals(flight.departureDate)
-                && arrivalDate.equals(flight.arrivalDate);
+        return Objects.equals(departureAirport, flight.departureAirport) && Objects.equals(arrivalAirport, flight.arrivalAirport);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(number);
+        return Objects.hash(departureAirport, arrivalAirport);
     }
 }
 
