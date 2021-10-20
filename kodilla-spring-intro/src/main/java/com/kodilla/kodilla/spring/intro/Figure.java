7b017d4ca8d35b718f9fc44d9c884be4db1e2f62
donzdanowicz <madpz89@gmail.com>
2021-10-20 11:25:44 +0200

    Task 14.1

---
+++ kodilla-spring-intro/src/main/java/com/kodilla/kodilla/spring/intro/Figure.java
@@ -0,0 +1,7 @@
+package com.kodilla.kodilla.spring.intro;
+
+public class Figure {
+    public void draw() {
+        System.out.println("This is a geometric figure.");
+    }
+}
---
+++ kodilla-spring-intro/src/main/java/com/kodilla/kodilla/spring/intro/Shape.java
@@ -0,0 +1,5 @@
+package com.kodilla.kodilla.spring.intro;
+
+public interface Shape {
+    String draw();
+}
