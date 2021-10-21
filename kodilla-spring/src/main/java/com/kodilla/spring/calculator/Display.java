ead013745a0e8185fa0b0a9f43a1110e9d8663a2
donzdanowicz <madpz89@gmail.com>
2021-10-21 22:43:02 +0200

    Task 14.2 added

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public final class Display {
-    public void displayValue(double val) {
+    public void displayValue(double val)
+    {
         System.out.println(val);
     }
 }
