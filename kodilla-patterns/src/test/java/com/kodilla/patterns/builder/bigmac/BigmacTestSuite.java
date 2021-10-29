3d252ae8d511507482c7e1ca199808d08adb1bd3
donzdanowicz <madpz89@gmail.com>
2021-10-29 17:21:57 +0200

    Task 15.5 Builder added

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,25 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BigmacTestSuite {
+    @Test
+    void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("with sesame")
+                .burgers(2)
+                .sauce("1000 Islands")
+                .ingredient("bacon")
+                .ingredient("mushrooms")
+                .ingredient("chilli peppers")
+                .ingredient("cheese")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        assertEquals(4, howManyIngredients);
+    }
+}
