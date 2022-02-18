4e848c47a3174319240b3964fcbaf5f34fa11651
donzdanowicz <madpz89@gmail.com>
2022-02-18 11:35:34 +0100

    Task 30.3

--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -5,30 +5,37 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public enum DbManager {
+//public enum DbManager {
+public class DbManager {
 
-    INSTANCE;
+    //INSTANCE;
 
     private Connection conn;
+    private static DbManager dbManagerInstance;
 
-    DbManager() {
+    private DbManager() throws SQLException {
         Properties connectionProps = new Properties();
         connectionProps.put("user", "kodilla_user");
         connectionProps.put("password", "kodilla_Pass123");
 
-        try {
+        //try {
             conn = DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/kodilla_course" +
                             "?serverTimezone=Europe/Warsaw" +
-                            "&useSSL=False",
+                            //&useSSL=False",
+                            "&allowPublicKeyRetrieval=true&useSSL=False",
                     connectionProps);
-        } catch (SQLException e) {
-            throw new ExceptionInInitializerError(e);
-        }
+        //} catch (SQLException e) {
+        //    throw new ExceptionInInitializerError(e);
+        //}
     }
 
-    public static DbManager getInstance() {
-        return INSTANCE;
+    public static DbManager getInstance() throws SQLException {
+        //return INSTANCE;
+        if (dbManagerInstance == null) {
+            dbManagerInstance = new DbManager();
+        }
+        return dbManagerInstance;
     }
 
     public Connection getConnection() {
