5e37260f72b80d9db76e79daa5282687aa801f1b
donzdanowicz <madpz89@gmail.com>
2022-03-03 21:38:33 +0100

    Task 31.2

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -0,0 +1,58 @@
+package com.kodilla.testing2.facebook;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+
+    public static final String XPATH_CREATE_NEW_ACCOUNT = "//div[contains(@class, \"_6ltg\")]/a[1]";
+    public static final String XPATH_COOKIES_BUTTON = "//div[contains(@class, \"9xo5\")]//button[1]";
+    public static final String XPATH_WAIT_FOR = "//div[contains(@class, \"9xo5\")]";
+    public static final String XPATH_REGISTER_NAME = "//div[contains(@class, \"5dbb\")]/input[0]";
+    public static final String XPATH_REGISTER_SURNAME = "//div[contains(@class, " +
+            "\"uiStickyPlaceholderInput uiStickyPlaceholderEmptyInput\")]/input[0]";
+    //public static final String XPATH_DAY = "//div[contains(@class, \"_5k_5\")]/span/span/select[1]";
+    public static final String XPATH_DAY = "//span[contains(@class, \"_5k_4\")]/span/select[0]";
+    public static final String XPATH_MONTH = "//span[contains(@class, \"_5k_4\")]/span/select[1]";
+    public static final String XPATH_YEAR = "//span[contains(@class, \"_5k_4\")]/span/select[2]";
+
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://facebook.com");
+
+        while (!driver.findElement(By.xpath(XPATH_WAIT_FOR)).isDisplayed()) ;
+
+        //Thread.sleep(5000);
+
+        WebElement cookies = driver.findElement(By.xpath(XPATH_COOKIES_BUTTON));
+        cookies.click();
+
+        WebElement register = driver.findElement(By.xpath(XPATH_CREATE_NEW_ACCOUNT));
+        register.click();
+
+       /* WebElement registerName = driver.findElement(By.xpath(XPATH_REGISTER_NAME));
+        registerName.sendKeys("Jack");
+
+        WebElement registerSurname = driver.findElement(By.xpath(XPATH_REGISTER_SURNAME));
+        registerSurname.sendKeys("Black");*/
+
+        WebElement selectDayCombo = driver.findElement(By.xpath(XPATH_DAY));
+        Select selectDay = new Select(selectDayCombo);
+        selectDay.selectByIndex(19);
+
+        WebElement selectMonthCombo = driver.findElement(By.xpath(XPATH_MONTH));
+        Select selectMonth = new Select(selectMonthCombo);
+        selectMonth.selectByIndex(6);
+
+        WebElement selectYearCombo = driver.findElement(By.xpath(XPATH_YEAR));
+        Select selectYear = new Select(selectYearCombo);
+        selectYear.selectByIndex(1979);
+
+    }
+}
